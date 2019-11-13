3f2e995cdd30115d640c3235ecee0e49d234c932
suzy <44599863+suzysaghir@users.noreply.github.com>
2019-11-13 10:16:42 +0300

    Update SpreadSyntax.js

--- SpreadSyntax.js
+++ SpreadSyntax.js
@@ -11,3 +11,10 @@ arr = [...array2, ...array1]; // arr = [3,4,5,0,1,2]
 var obj1 = { foo: 'bar', x: 42 };
 var obj2 = { foo: 'bar', y: 13 };
 var mergedObj = { ...obj1, ...obj2 }; // { foo: "baz", x: 42, y: 13 } 'worked on ES5 not always on ES6 (depend on browser)'
+
+
+var obj1 = { foo: 'bar', x: 42 };
+var obj2 = { foo: 'baz', y: 13 };
+const merge = ( ...objects ) => ( { ...objects } );
+var mergedObj = merge ( {}, obj1, obj2); // { 0: {}, 1: { foo: 'bar', x: 42 }, 2: { foo: 'baz', y: 13 } }
+
