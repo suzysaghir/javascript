427aa09b94ae173a5e2fd3a471547dcfff4e12a4
suzy <44599863+suzysaghir@users.noreply.github.com>
2019-11-13 14:05:05 +0300

    Create objectShorthand.js

---
+++ objectShorthand.js
@@ -0,0 +1,21 @@
+
+
+//declare an object 
+var cat = ' ';
+var dog = 'Woof';
+var bird = 'Peet peet';
+
+var someObject = {
+  cat: cat,
+  dog: dog,
+  bird: bird
+}
+
+//shorhand declaration
+var someObject = {
+  cat,
+  dog ,
+  bird 
+}
+
+someObject.cat // 'Miaow'
