ba74a644d28172c8a41e5db428c6c31642d3c7a3
suzy <44599863+suzysaghir@users.noreply.github.com>
2019-11-13 10:02:39 +0300

    Update SpreadSyntax.js

--- SpreadSyntax.js
+++ SpreadSyntax.js
@@ -1,13 +1,13 @@
+
+const str = 'a random string'; 
+const char = [ ...str ]; // ["a"," ","r","a","n","d","o","m"," ","s","t","r","i","n","g"] 
+
+
 var array1 = [0, 1, 2];
 var array2 = [3, 4, 5];
 arr = [...array2, ...array1]; // arr = [3,4,5,0,1,2]
 
 
 var obj1 = { foo: 'bar', x: 42 };
-var obj2 = { foo: 'baz', y: 13 };
-
-
-var mergedObj = { ...obj1, ...obj2 };
-// Object { foo: "baz", x: 42, y: 13 }
-
-
+var obj2 = { foo: 'bar', y: 13 };
+var mergedObj = { ...obj1, ...obj2 }; // { foo: "baz", x: 42, y: 13 } 'worked on ES5 not always on ES6 (depend on browser)'
