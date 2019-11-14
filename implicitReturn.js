c8ccd46a895308a89a4dba6e4cbebbbbc75d4bb7
suzy <44599863+suzysaghir@users.noreply.github.com>
2019-11-14 11:09:12 +0300

    Create implicitReturn.js

---
+++ implicitReturn.js
@@ -0,0 +1,13 @@
+//Implicit conversions
+var ageGreeting = "I am " + 9 + " years old." // "I am 9 years old." 
+
+//Explicit conversions
+var numString = "100";
+var num = Number(numString);
+
+
+//Implicit Return: without return and paranthisi
+const foo = x => x + 1;
+  //for object should use ()
+const foo = () => ({ bar: 1 }) 
+
